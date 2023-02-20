39d5a5d9f20395b550103121847517d125b1b930
Anandmohanty <mohantyanand3@gmail.com>
2023-02-20 12:02:15 +0530

    [Anand] - Editing Contact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -3,19 +3,23 @@
     
         internal class Program
         {
-        internal class Program
-        {
-            static void Main(string[] args)
-            {   //print Message
-                Console.WriteLine("Welcome to Address Book  System");
+        static void Main(string[] args)
+        {   //print Message
+            Console.WriteLine("Welcome to Address Book  System");
 
-                //Create contact
-                CreateContact.PersonDetails();
-                Console.ReadLine();
+            //Create contact
+            CreateContact.PersonDetails();
+            Console.ReadLine();
 
-                //add a new contact
-                AddContact.GetCustomer();
-                AddContact.ListingPeople();
-            }
+            //add a new contact
+            AddContact.GetCustomer();
+            AddContact.ListingPeople();
+
+            // Modify existing contact
+            EditContact.GetCustomer();
+            EditContact.Modify();
+            EditContact.ListingPeople();
+
         }
+    }
     }
