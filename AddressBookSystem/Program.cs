e5fd63aa9c6c8a02a893f4dd4f498e38b54780a6
Anandmohanty <mohantyanand3@gmail.com>
2023-02-20 11:53:29 +0530

    [Anand] - Add new Contact

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBookSystem
+{
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {   //print Message
+                Console.WriteLine("Welcome to AddressBook Book System");
+
+                //add a new contact
+                CreateContact.PersonDetails();
+                Console.ReadLine();
+            }
+        }
+    }
+}
